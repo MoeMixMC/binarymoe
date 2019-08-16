63f10c8f32c342eef638645dc28a0632ad427029
moemi <moabdalla246@gmail.com>
2019-08-16 00:30:22 -0600

    Toolbar needs work...

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Section1 from './SectionComponents/section1.js'
 import Section2 from './SectionComponents/section2.js'
 import Section3 from './SectionComponents/section3.js'
+import Toolbar from './Toolbar.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <main class="wrapper">
+      <Toolbar></Toolbar>
       <Section1></Section1>
       <Section2></Section2>
       <Section3></Section3>
---
+++ src/Toolbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Toolbar() {
+  return (
+    <div className="Toolbar">
+        hi
+    </div>
+  );
+}
+
+export default Toolbar;
